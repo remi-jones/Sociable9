00c996622a8e31c84ac81237fd348a1f4b74d0db
remi-jones <erj7171@umoncton.ca>
2023-12-09 12:23:10 -0400

    update IdentityDataInitializer class with details

--- Sociable9/Models/IdentityDataInitializer.cs
+++ Sociable9/Models/IdentityDataInitializer.cs
@@ -2,16 +2,26 @@
 
 namespace Sociable9.Models
 {
+    // IdentityDataInitializer class :
+    // used to seed the database with the admin user
+    // the class will create the admin user with the following information :
+    // UserName : admin
+    // Email : admin@sociable.com (will be verified automatically)
+    // password : Appleapplecak3!
+    // the class will also create the Admin Role in the database
+    // the class will also associate the admin user with the Admin Role.
     public class IdentityDataInitializer
     {
         public static async Task SeedData(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            await CreateRoles(roleManager);
-            await CreateAdminUser(userManager);
-            await AssignAdminRole(userManager);
+            await CreateRoles(roleManager); 
+            await CreateAdminUser(userManager); 
+            await AssignAdminRole(userManager); 
         }
         private static async Task CreateRoles(RoleManager<IdentityRole> roleManager)
         {
+            
+            // create admin role
             if (!await roleManager.RoleExistsAsync("Admin"))
             {
                 await roleManager.CreateAsync(new IdentityRole("Admin"));
@@ -19,6 +29,7 @@ namespace Sociable9.Models
         }
         private static async Task CreateAdminUser(UserManager<IdentityUser> userManager)
         {
+            // create admin user
             var adminUser = new IdentityUser
             {
                 UserName = "admin",
@@ -34,6 +45,7 @@ namespace Sociable9.Models
 
         private static async Task AssignAdminRole(UserManager<IdentityUser> userManager)
         {
+            // associate admin user to admin role
             var adminUser = await userManager.FindByEmailAsync("admin@sociable.com");
             if (adminUser != null && !await userManager.IsInRoleAsync(adminUser, "Admin"))
             {
